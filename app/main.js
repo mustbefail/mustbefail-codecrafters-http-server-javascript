9a58a1b3680be7794aa477196b8d579a05adf916
Viktor Kushnir <viktor.kushnir@djangostars.com>
2023-11-08 18:11:10 +0200

    pass 3rd stage

--- app/main.js
+++ app/main.js
@@ -6,7 +6,15 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
-    socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    const dataString = data.toString();
+    const [startLine] = dataString.trim().split("\r\n");
+
+    if(startLine.split(" ").includes("/")) {
+      socket.write("HTTP/1.1 200 OK\r\n\r\n")
+    } else {
+      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+    }
+
     socket.end();
   });
   socket.on("close", () => {
