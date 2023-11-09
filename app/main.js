e03343ea455b2bb638facf2195f5845675ce4ce0
Viktor Kushnir <viktor.kushnir@djangostars.com>
2023-11-09 21:59:54 +0200

    stage 4

--- app/main.js
+++ app/main.js
@@ -6,16 +6,21 @@ console.log("Logs from your program will appear here!");
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on("data", (data) => {
+    const pattern = new RegExp(/\/(?<=\/echo\/)(.*)/);
     const dataString = data.toString();
     const [startLine] = dataString.trim().split("\r\n");
     const [method, path, version] = startLine.split(" ");
+    const res = path.match(pattern)?.[1];
 
-    if(path === "/") {
-      socket.write("HTTP/1.1 200 OK\r\n\r\n")
-    } else {
-      socket.write("HTTP/1.1 404 Not Found\r\n\r\n")
+    if(path === '/') {
+      socket.write(`HTTP/1.1 200 OK\r\n`);
+      socket.end();
     }
-    socket.end();
+
+    if(res) {
+      socket.write(`HTTP/1.1 200 OK\r\nContent-Type: test/plain\r\n\r\n${res}`)
+      socket.end();
+    }
   });
   socket.on("close", () => {
     socket.end();
